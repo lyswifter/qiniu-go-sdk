bc1b13f6eb76f7e69d688597aebcc0cc50108982
Bachue Zhou <bachue.shu@gmail.com>
2021-10-29 17:00:52 +0800

    support multi clusters

--- syncdata/operation/config.go
+++ syncdata/operation/config.go
@@ -3,18 +3,15 @@ package operation
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io/ioutil"
-	"os"
 	"path"
-	"path/filepath"
 	"strings"
-	"sync"
 
-	"github.com/fsnotify/fsnotify"
 	"github.com/pelletier/go-toml"
-	"github.com/qiniupd/qiniu-go-sdk/x/log.v7"
 )
+
+// 单集群配置文件
+type SingleClusterConfig = Config
 
 // 配置文件
 type Config struct {
@@ -36,15 +33,24 @@ type Config struct {
 
 	IoHosts []string `json:"io_hosts" toml:"io_hosts"`
 	UcHosts []string `json:"uc_hosts" toml:"uc_hosts"`
+
+	originalPath string `json:"-" toml:"-"`
 }
 
-func dupStrings(s []string) []string {
-	if s == nil || len(s) == 0 {
-		return s
+func (config *Config) forEachClusterConfig(f func(*Config) error) error {
+	return f(config)
+}
+
+func (config *Config) forKey(key string) (*Config, bool) {
+	return config, true
+}
+
+func (config *Config) getOriginalPaths() []string {
+	paths := make([]string, 0, 1)
+	if config.originalPath != "" {
+		paths = append(paths, config.originalPath)
 	}
-	to := make([]string, len(s))
-	copy(to, s)
-	return to
+	return paths
 }
 
 // 加载配置文件
@@ -54,99 +60,15 @@ func Load(file string) (*Config, error) {
 	if err != nil {
 		return nil, err
 	}
-	ext := path.Ext(file)
-	ext = strings.ToLower(ext)
+	ext := strings.ToLower(path.Ext(file))
 	if ext == ".json" {
 		err = json.Unmarshal(raw, &configuration)
 	} else if ext == ".toml" {
 		err = toml.Unmarshal(raw, &configuration)
 	} else {
-		return nil, errors.New("configuration format invalid!")
+		return nil, errors.New("invalid configuration format")
 	}
+	configuration.originalPath = file
 
 	return &configuration, err
-}
-
-var g_conf *Config
-var confLock sync.Mutex
-
-func getConf() *Config {
-	up := os.Getenv("QINIU")
-	if up == "" {
-		log.Warn("not set qiniu environment")
-		return nil
-	}
-	confLock.Lock()
-	defer confLock.Unlock()
-	if g_conf != nil {
-		return g_conf
-	}
-	c, err := Load(up)
-	if err != nil {
-		log.Warn("load conf failed", up, err)
-		return nil
-	}
-	g_conf = c
-	watchConfig(up)
-	return c
-}
-
-func watchConfig(filename string) {
-	initWG := sync.WaitGroup{}
-	initWG.Add(1)
-	go func() {
-		watcher, err := fsnotify.NewWatcher()
-		if err != nil {
-			elog.Fatal(err)
-		}
-		defer watcher.Close()
-
-		configFile := filepath.Clean(filename)
-		configDir, _ := filepath.Split(configFile)
-		realConfigFile, _ := filepath.EvalSymlinks(filename)
-
-		eventsWG := sync.WaitGroup{}
-		eventsWG.Add(1)
-		go func() {
-			for {
-				select {
-				case event, ok := <-watcher.Events:
-					if !ok { // 'Events' channel is closed
-						eventsWG.Done()
-						return
-					}
-					currentConfigFile, _ := filepath.EvalSymlinks(filename)
-					// we only care about the config file with the following cases:
-					// 1 - if the config file was modified or created
-					// 2 - if the real path to the config file changed (eg: k8s ConfigMap replacement)
-					const writeOrCreateMask = fsnotify.Write | fsnotify.Create
-					if (filepath.Clean(event.Name) == configFile &&
-						event.Op&writeOrCreateMask != 0) ||
-						(currentConfigFile != "" && currentConfigFile != realConfigFile) {
-						realConfigFile = currentConfigFile
-						c, err := Load(realConfigFile)
-						fmt.Printf("re reading config file: error %v\n", err)
-						if err == nil {
-							g_conf = c
-						}
-					} else if filepath.Clean(event.Name) == configFile &&
-						event.Op&fsnotify.Remove&fsnotify.Remove != 0 {
-						eventsWG.Done()
-						return
-					}
-
-				case err, ok := <-watcher.Errors:
-					if ok { // 'Errors' channel is not closed
-						fmt.Printf("watcher error: %v\n", err)
-					}
-					eventsWG.Done()
-					return
-				}
-			}
-		}()
-		watcher.Add(configDir)
-		initWG.Done()   // done initializing the watch in this go routine, so the parent routine can move on...
-		eventsWG.Wait() // now, wait for event loop to end in this go-routine...
-	}()
-	initWG.Wait() // make sure that the go routine above fully ended before returning
 }
--- syncdata/operation/list.go
+++ syncdata/operation/list.go
@@ -3,7 +3,10 @@ package operation
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"io"
+	"os"
+	"strconv"
 	"sync"
 	"sync/atomic"
 
@@ -11,8 +14,255 @@ import (
 	"github.com/qiniupd/qiniu-go-sdk/api.v7/kodo"
 )
 
+var (
+	ErrUndefinedConfig                        = errors.New("undefined config")
+	ErrCannotTransferBetweenDifferentClusters = errors.New("cannot transfer between different clusters")
+)
+
 // 列举器
 type Lister struct {
+	config                   Configurable
+	singleClusterLister      *singleClusterLister
+	multiClustersConcurrency int
+}
+
+// 根据配置创建列举器
+func NewLister(c *Config) *Lister {
+	return &Lister{config: c, singleClusterLister: newSingleClusterLister(c)}
+}
+
+// 根据环境变量创建列举器
+func NewListerV2() *Lister {
+	c := getCurrentConfigurable()
+	if c == nil {
+		return nil
+	} else if singleClusterConfig, ok := c.(*Config); ok {
+		return NewLister(singleClusterConfig)
+	} else {
+		var (
+			concurrency = 1
+			err         error
+		)
+		if concurrencyStr := os.Getenv("QINIU_MULTI_CLUSTERS_CONCURRENCY"); concurrencyStr != "" {
+			if concurrency, err = strconv.Atoi(concurrencyStr); err != nil {
+				elog.Warn("Invalid QINIU_MULTI_CLUSTERS_CONCURRENCY: ", err)
+			}
+		}
+		return &Lister{config: c, multiClustersConcurrency: concurrency}
+	}
+}
+
+// 文件元信息
+type FileStat struct {
+	Name string `json:"name"`
+	Size int64  `json:"size"`
+}
+
+// 重命名对象
+func (l *Lister) Rename(fromKey, toKey string) error {
+	var scl *singleClusterLister
+	if l.singleClusterLister != nil {
+		scl = l.singleClusterLister
+	} else {
+		c, err := l.canTransfer(fromKey, toKey)
+		if err != nil {
+			return err
+		}
+		scl = newSingleClusterLister(c)
+	}
+	return scl.rename(fromKey, toKey)
+}
+
+// 移动对象到指定存储空间的指定对象中
+func (l *Lister) MoveTo(fromKey, toBucket, toKey string) error {
+	var scl *singleClusterLister
+	if l.singleClusterLister != nil {
+		scl = l.singleClusterLister
+	} else {
+		c, err := l.canTransfer(fromKey, toKey)
+		if err != nil {
+			return err
+		}
+		scl = newSingleClusterLister(c)
+	}
+	return scl.moveTo(fromKey, toBucket, toKey)
+}
+
+// 复制对象到当前存储空间的指定对象中
+func (l *Lister) Copy(fromKey, toKey string) error {
+	var scl *singleClusterLister
+	if l.singleClusterLister != nil {
+		scl = l.singleClusterLister
+	} else {
+		c, err := l.canTransfer(fromKey, toKey)
+		if err != nil {
+			return err
+		}
+		scl = newSingleClusterLister(c)
+	}
+	return scl.copy(fromKey, toKey)
+}
+
+func (l *Lister) canTransfer(fromKey, toKey string) (*Config, error) {
+	configOfFromKey, exists := l.config.forKey(fromKey)
+	if !exists {
+		return nil, ErrUndefinedConfig
+	}
+	configOfToKey, exists := l.config.forKey(toKey)
+	if !exists {
+		return nil, ErrUndefinedConfig
+	}
+	if configOfFromKey != configOfToKey {
+		return nil, ErrCannotTransferBetweenDifferentClusters
+	}
+	return configOfFromKey, nil
+}
+
+// 删除指定对象
+func (l *Lister) Delete(key string) error {
+	var scl *singleClusterLister
+	if l.singleClusterLister != nil {
+		scl = l.singleClusterLister
+	} else {
+		c, exists := l.config.forKey(key)
+		if !exists {
+			return ErrUndefinedConfig
+		}
+		scl = newSingleClusterLister(c)
+	}
+	return scl.delete(key)
+}
+
+// 获取指定对象列表的元信息
+func (l *Lister) ListStat(keys []string) []*FileStat {
+	if fileStats, err := l.listStat(context.Background(), keys); err != nil {
+		return []*FileStat{}
+	} else {
+		return fileStats
+	}
+}
+
+func (l *Lister) listStat(ctx context.Context, keys []string) ([]*FileStat, error) {
+	if l.singleClusterLister != nil {
+		return l.singleClusterLister.listStat(ctx, keys)
+	}
+
+	type KeysWithIndex struct {
+		IndexMap []int
+		Keys     []string
+	}
+
+	clusterPathsMap := make(map[*Config]*KeysWithIndex)
+	for i, key := range keys {
+		config, exists := l.config.forKey(key)
+		if !exists {
+			return nil, ErrUndefinedConfig
+		}
+		if keysWithIndex := clusterPathsMap[config]; keysWithIndex != nil {
+			keysWithIndex.IndexMap = append(keysWithIndex.IndexMap, i)
+			keysWithIndex.Keys = append(keysWithIndex.Keys, key)
+		} else {
+			keysWithIndex = &KeysWithIndex{Keys: make([]string, 0, 1), IndexMap: make([]int, 0, 1)}
+			keysWithIndex.IndexMap = append(keysWithIndex.IndexMap, i)
+			keysWithIndex.Keys = append(keysWithIndex.Keys, key)
+			clusterPathsMap[config] = keysWithIndex
+		}
+	}
+
+	pool := newGoroutinePool(l.multiClustersConcurrency)
+	allStats := make([]*FileStat, len(keys))
+	for config, keysWithIndex := range clusterPathsMap {
+		func(config *Config, keys []string, indexMap []int) {
+			pool.Go(func(ctx context.Context) error {
+				if stats, err := l.listStatForConfig(ctx, config, keys); err != nil {
+					return err
+				} else {
+					for i := range stats {
+						allStats[indexMap[i]] = stats[i]
+					}
+					return nil
+				}
+			})
+		}(config, keysWithIndex.Keys, keysWithIndex.IndexMap)
+	}
+	err := pool.Wait(context.Background())
+	return allStats, err
+}
+
+func (l *Lister) listStatForConfig(ctx context.Context, config *Config, keys []string) ([]*FileStat, error) {
+	return newSingleClusterLister(config).listStat(ctx, keys)
+}
+
+// 根据前缀列举存储空间
+func (l *Lister) ListPrefix(prefix string) []string {
+	if keys, err := l.listPrefix(context.Background(), prefix); err != nil {
+		return []string{}
+	} else {
+		return keys
+	}
+}
+
+func (l *Lister) listPrefix(ctx context.Context, prefix string) ([]string, error) {
+	if l.singleClusterLister != nil {
+		return l.singleClusterLister.listPrefix(ctx, prefix)
+	}
+
+	pool := newGoroutinePool(l.multiClustersConcurrency)
+	allKeys := make([]string, 0)
+	var allKeysMutex sync.Mutex
+	l.config.forEachClusterConfig(func(config *Config) error {
+		pool.Go(func(ctx context.Context) error {
+			if keys, err := l.listPrefixForConfig(ctx, config, prefix); err != nil {
+				return err
+			} else {
+				allKeysMutex.Lock()
+				allKeys = append(allKeys, keys...)
+				allKeysMutex.Unlock()
+				return nil
+			}
+		})
+		return nil
+	})
+	err := pool.Wait(ctx)
+	return allKeys, err
+}
+
+func (l *Lister) listPrefixForConfig(ctx context.Context, config *Config, prefix string) ([]string, error) {
+	return newSingleClusterLister(config).listPrefix(ctx, prefix)
+}
+
+func newSingleClusterLister(c *Config) *singleClusterLister {
+	mac := qbox.NewMac(c.Ak, c.Sk)
+
+	var queryer *Queryer = nil
+
+	if len(c.UcHosts) > 0 {
+		queryer = NewQueryer(c)
+	}
+
+	lister := singleClusterLister{
+		bucket:           c.Bucket,
+		rsHosts:          dupStrings(c.RsHosts),
+		upHosts:          dupStrings(c.UpHosts),
+		rsfHosts:         dupStrings(c.RsfHosts),
+		credentials:      mac,
+		queryer:          queryer,
+		batchConcurrency: c.BatchConcurrency,
+		batchSize:        c.BatchSize,
+	}
+	if lister.batchConcurrency <= 0 {
+		lister.batchConcurrency = 20
+	}
+	if lister.batchSize <= 0 {
+		lister.batchSize = 100
+	}
+	shuffleHosts(lister.rsHosts)
+	shuffleHosts(lister.rsfHosts)
+	shuffleHosts(lister.upHosts)
+	return &lister
+}
+
+type singleClusterLister struct {
 	bucket           string
 	rsHosts          []string
 	upHosts          []string
@@ -23,26 +273,9 @@ type Lister struct {
 	batchConcurrency int
 }
 
-// 文件元信息
-type FileStat struct {
-	Name string `json:"name"`
-	Size int64  `json:"size"`
-}
-
-func (l *Lister) batchStat(r io.Reader) []*FileStat {
-	j := json.NewDecoder(r)
-	var fl []string
-	err := j.Decode(&fl)
-	if err != nil {
-		elog.Error(err)
-		return nil
-	}
-	return l.ListStat(fl)
-}
-
 var curRsHostIndex uint32 = 0
 
-func (l *Lister) nextRsHost(failedHosts map[string]struct{}) string {
+func (l *singleClusterLister) nextRsHost(failedHosts map[string]struct{}) string {
 	rsHosts := l.rsHosts
 	if l.queryer != nil {
 		if hosts := l.queryer.QueryRsHosts(false); len(hosts) > 0 {
@@ -70,7 +303,7 @@ func (l *Lister) nextRsHost(failedHosts map[string]struct{}) string {
 
 var curRsfHostIndex uint32 = 0
 
-func (l *Lister) nextRsfHost(failedHosts map[string]struct{}) string {
+func (l *singleClusterLister) nextRsfHost(failedHosts map[string]struct{}) string {
 	rsfHosts := l.rsfHosts
 	if l.queryer != nil {
 		if hosts := l.queryer.QueryRsfHosts(false); len(hosts) > 0 {
@@ -96,8 +329,7 @@ func (l *Lister) nextRsfHost(failedHosts map[string]struct{}) string {
 	}
 }
 
-// 重命名对象
-func (l *Lister) Rename(fromKey, toKey string) error {
+func (l *singleClusterLister) rename(fromKey, toKey string) error {
 	failedRsHosts := make(map[string]struct{})
 	host := l.nextRsHost(failedRsHosts)
 	bucket := l.newBucket(host, "")
@@ -123,8 +355,7 @@ func (l *Lister) Rename(fromKey, toKey string) error {
 	return nil
 }
 
-// 移动对象到指定存储空间的指定对象中
-func (l *Lister) MoveTo(fromKey, toBucket, toKey string) error {
+func (l *singleClusterLister) moveTo(fromKey, toBucket, toKey string) error {
 	failedRsHosts := make(map[string]struct{})
 	host := l.nextRsHost(failedRsHosts)
 	bucket := l.newBucket(host, "")
@@ -150,8 +381,7 @@ func (l *Lister) MoveTo(fromKey, toBucket, toKey string) error {
 	return nil
 }
 
-// 复制对象到当前存储空间的指定对象中
-func (l *Lister) Copy(fromKey, toKey string) error {
+func (l *singleClusterLister) copy(fromKey, toKey string) error {
 	failedRsHosts := make(map[string]struct{})
 	host := l.nextRsHost(failedRsHosts)
 	bucket := l.newBucket(host, "")
@@ -177,8 +407,7 @@ func (l *Lister) Copy(fromKey, toKey string) error {
 	return nil
 }
 
-// 删除指定对象
-func (l *Lister) Delete(key string) error {
+func (l *singleClusterLister) delete(key string) error {
 	failedRsHosts := make(map[string]struct{})
 	host := l.nextRsHost(failedRsHosts)
 	bucket := l.newBucket(host, "")
@@ -204,8 +433,7 @@ func (l *Lister) Delete(key string) error {
 	return nil
 }
 
-// 获取指定对象列表的元信息
-func (l *Lister) ListStat(paths []string) []*FileStat {
+func (l *singleClusterLister) listStat(ctx context.Context, paths []string) ([]*FileStat, error) {
 	concurrency := (len(paths) + l.batchSize - 1) / l.batchSize
 	if concurrency > l.batchConcurrency {
 		concurrency = l.batchConcurrency
@@ -265,15 +493,11 @@ func (l *Lister) ListStat(paths []string) []*FileStat {
 			})
 		}(paths[i:i+size], i)
 	}
-	if err := pool.Wait(context.Background()); err != nil {
-		return []*FileStat{}
-	} else {
-		return stats
-	}
+	err := pool.Wait(ctx)
+	return stats, err
 }
 
-// 根据前缀列举存储空间
-func (l *Lister) ListPrefix(prefix string) []string {
+func (l *singleClusterLister) listPrefix(ctx context.Context, prefix string) ([]string, error) {
 	failedHosts := make(map[string]struct{})
 	rsHost := l.nextRsHost(failedHosts)
 	rsfHost := l.nextRsfHost(failedHosts)
@@ -281,19 +505,19 @@ func (l *Lister) ListPrefix(prefix string) []string {
 	var files []string
 	marker := ""
 	for {
-		r, _, out, err := bucket.List(nil, prefix, "", marker, 1000)
+		r, _, out, err := bucket.List(ctx, prefix, "", marker, 1000)
 		if err != nil && err != io.EOF {
 			failedHosts[rsfHost] = struct{}{}
 			failHostName(rsfHost)
 			elog.Info("ListPrefix retry 0", rsfHost, err)
 			rsfHost = l.nextRsfHost(failedHosts)
 			bucket = l.newBucket(rsHost, rsfHost)
-			r, _, out, err = bucket.List(nil, prefix, "", "", 1000)
-			if err != nil {
+			r, _, out, err = bucket.List(ctx, prefix, "", marker, 1000)
+			if err != nil && err != io.EOF {
 				failedHosts[rsfHost] = struct{}{}
 				failHostName(rsfHost)
 				elog.Info("ListPrefix retry 1", rsfHost, err)
-				return []string{}
+				return nil, err
 			} else {
 				succeedHostName(rsfHost)
 			}
@@ -310,51 +534,10 @@ func (l *Lister) ListPrefix(prefix string) []string {
 		}
 		marker = out
 	}
-	return files
+	return files, nil
 }
 
-// 根据配置创建列举器
-func NewLister(c *Config) *Lister {
-	mac := qbox.NewMac(c.Ak, c.Sk)
-
-	var queryer *Queryer = nil
-
-	if len(c.UcHosts) > 0 {
-		queryer = NewQueryer(c)
-	}
-
-	lister := Lister{
-		bucket:           c.Bucket,
-		rsHosts:          dupStrings(c.RsHosts),
-		upHosts:          dupStrings(c.UpHosts),
-		rsfHosts:         dupStrings(c.RsfHosts),
-		credentials:      mac,
-		queryer:          queryer,
-		batchConcurrency: c.BatchConcurrency,
-		batchSize:        c.BatchSize,
-	}
-	if lister.batchConcurrency <= 0 {
-		lister.batchConcurrency = 20
-	}
-	if lister.batchSize <= 0 {
-		lister.batchSize = 100
-	}
-	shuffleHosts(lister.rsHosts)
-	shuffleHosts(lister.rsfHosts)
-	shuffleHosts(lister.upHosts)
-	return &lister
-}
-
-// 根据环境变量创建列举器
-func NewListerV2() *Lister {
-	c := getConf()
-	if c == nil {
-		return nil
-	}
-	return NewLister(c)
-}
-
-func (l *Lister) newBucket(host, rsfHost string) kodo.Bucket {
+func (l *singleClusterLister) newBucket(host, rsfHost string) kodo.Bucket {
 	cfg := kodo.Config{
 		AccessKey: l.credentials.AccessKey,
 		SecretKey: string(l.credentials.SecretKey),
@@ -364,4 +547,15 @@ func (l *Lister) newBucket(host, rsfHost string) kodo.Bucket {
 	}
 	client := kodo.NewWithoutZone(&cfg)
 	return client.Bucket(l.bucket)
+}
+
+func (l *Lister) batchStab(r io.Reader) []*FileStat {
+	j := json.NewDecoder(r)
+	var fl []string
+	err := j.Decode(&fl)
+	if err != nil {
+		elog.Error(err)
+		return nil
+	}
+	return l.ListStat(fl)
 }
